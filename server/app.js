59e542dc7d17b7ad515f3172b3e7be3f76ae5207
RuyuYou <727809166@qq.com>
2017-04-08 22:12:16 +0800

    add mongo

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
+const config = require('config');
+
+mongoose.connect(config.get('mongoUri'));
 
 const app = express();
 
 app.use(express.static('public'));
-
 
 
 app.use('*', (req, res)=> {

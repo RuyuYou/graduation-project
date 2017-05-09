d5a1cf6fbb8d23706f3093ec57261a11c77f9170
RuyuYou <727809166@qq.com>
2017-05-09 15:25:35 +0800

    new commit

--- client/src/component/trainEditor/train-editor-body.js
+++ client/src/component/trainEditor/train-editor-body.js
@@ -3,7 +3,15 @@ import {Component} from 'react';
 export default class TrainEditorBody extends Component {
   render() {
     return (<div className="train-editor-body">
-      hello
+      <div className='form-group row'>
+        <label className='col-sm-4 control-label'> 列车号 </label>
+        <div className='col-sm-6'>
+          <input type='text' className='form-control' placeholder='请输入列车号'
+                 ref={(ref) => {
+                   this.trainId = ref;
+                 }}/>
+        </div>
+      </div>
     </div>);
   }
 }

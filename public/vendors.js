1373b6e229eba6d444aa765d19ea37fadeec1103
RuyuYou <727809166@qq.com>
2017-05-10 22:36:21 +0800

    new commit

--- public/vendors.js
+++ public/vendors.js
@@ -76,7 +76,7 @@
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + ({"0":"index"}[chunkId]||chunkId) + "." + {"0":"f51beda8"}[chunkId] + ".js";
+/******/ 			script.src = __webpack_require__.p + "" + ({"0":"index"}[chunkId]||chunkId) + "." + {"0":"ee45302f"}[chunkId] + ".js";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};

24003386a539c640ea1e8d15fa32351261405227
RuyuYou <727809166@qq.com>
2017-05-11 21:02:01 +0800

    new commit

--- client/src/component/trainEditor/index.js
+++ client/src/component/trainEditor/index.js
@@ -2,21 +2,33 @@ import {Component} from 'react';
 import TrainEditorHeader from './train-editor-header';
 import '../../../style/train-editor.less';
 import TrainEditorBody from './train-editor-body';
+import superagent from 'superagent';
+import noCache from 'superagent-no-cache';
 
 export default class TrainEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: ''
+      title: '',
+      trainInformation: {}
     }
   }
 
   componentDidMount() {
     const pathNameArray = window.location.pathname.split('/');
     if (pathNameArray[pathNameArray.length - 1] === 'edit') {
-      this.setState({
-        title: '修改'
-      });
+      superagent
+        .get(`/trains/${pathNameArray[2]}`)
+        .use(noCache)
+        .end((err, res)=> {
+          if (err) {
+            throw err;
+          }
+          this.setState({
+            title: '修改',
+            trainInformation: res.body
+          });
+        })
     } else {
       this.setState({
         title: '新增'
@@ -28,7 +40,8 @@ export default class TrainEditor extends Component {
     return (<div id="train-editor">
       <TrainEditorHeader title={this.state.title}/>
       <div className="train-editor-body">
-        <TrainEditorBody title={this.state.title}/>
+        <TrainEditorBody title={this.state.title}
+                         trainInformation={this.state.trainInformation}/>
       </div>
     </div>);
   }

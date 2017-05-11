1420e88f229661caf396d4bff5dd1fb1b5dfe2b5
RuyuYou <727809166@qq.com>
2017-05-11 20:31:30 +0800

    new commit

--- client/src/component/trainEditor/index.js
+++ client/src/component/trainEditor/index.js
@@ -4,9 +4,29 @@ import '../../../style/train-editor.less';
 import TrainEditorBody from './train-editor-body';
 
 export default class TrainEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ''
+    }
+  }
+
+  componentDidMount() {
+    const pathNameArray = window.location.pathname.split('/');
+    if (pathNameArray[pathNameArray.length - 1] === 'edit') {
+      this.setState({
+        title: '修改车次'
+      });
+    } else {
+      this.setState({
+        title: '新增车次'
+      })
+    }
+  }
+
   render() {
     return (<div id="train-editor">
-      <TrainEditorHeader/>
+      <TrainEditorHeader title={this.state.title}/>
       <div className="train-editor-body">
         <TrainEditorBody/>
       </div>
--- client/src/component/trainEditor/train-editor-header.js
+++ client/src/component/trainEditor/train-editor-header.js
@@ -2,18 +2,9 @@ import {Component} from 'react';
 
 export default class TrainEditorHeader extends Component {
 
-  judgeTitle() {
-    const pathNameArray = window.location.pathname.split('/');
-    if (pathNameArray[pathNameArray.length - 1] === 'edit') {
-      return '修改车次';
-    } else {
-      return '新增车次';
-    }
-  }
-
   render() {
     return (<div className="train-editor-header">
-      {this.judgeTitle()}
+      {this.props.title}
     </div>);
   }
 }

410c28f34abfba8e0a092a2721fb9cfc1a760431
RuyuYou <727809166@qq.com>
2017-05-16 17:00:50 +0800

    new commi

--- server/controllers/report-controller.js
+++ server/controllers/report-controller.js
@@ -1,47 +1,39 @@
 const Train = require('../models/train');
 const constant = require('../../config/constant');
 
-var students = [
-  {
-    name: 'a',
-    age: 1
-  }, {
-    name: 'b',
-    age: 2
-  }, {
-    name: 'c',
-    age: 3
-  }, {
-    name: 'd',
-    age: 4
-  }, {
-    name: 'e',
-    age: 5
-  }
-];
-
 class ReportController {
   getAll(req, res, next) {
-    // Train.find({}, (err, doc)=> {
-    //   if (err) {
-    //     return next(err);
-    //   }
-    //
-    // });
-    var content = '';
+    let content = '';
+    Train.find({}, (err, doc)=> {
+      if (err) {
+        return next(err);
+      }
+      content += '列车号,总时长,发车时间,始发站,终点站,中间站\n';
+      for (var i = 0; i < doc.length; i++) {
+        const lastedTime = `${doc[i].lastedTime.hour}时${doc[i].lastedTime.minutes}分`;
+        const startTime = `${doc[i].startTime.year}年${doc[i].startTime.month}月${doc[i].startTime.day}日${doc[i].startTime.hour}时${doc[i].startTime.minutes}分`;
+        let middleText = '';
+        const middlePlace = doc[i].middlePlace.map((item, index)=> {
+          if (index === 0) {
+            middleText += item;
+          } else {
+            middleText += `、${item}`;
+          }
+        });
+        content += doc[i].trainId + ',';
+        content += lastedTime + ',';
+        content += startTime + ',';
+        content += doc[i].startPlace + ',';
+        content += doc[i].endPlace + ',';
+        content += middleText + '\n';
+      }
+      var filename = 'train.csv';
 
-    content += '姓名,年龄\n';
-    for (var i = 0; i < students.length; ++i) {
+      res.setHeader('Content-disposition', 'attachment; filename=' + filename + '');
+      res.setHeader('Content-Type', 'train/csv');
 
-      content += students[i].name + ',';
-      content += students[i].age + '\n';
-    }
-    var filename = 'test.csv';
-
-    res.setHeader('Content-disposition', 'attachment; filename=' + filename + '');
-    res.setHeader('Content-Type', 'text/csv');
-
-    res.send(content);
+      res.send(content);
+    });
   }
 }
 

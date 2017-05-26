e4ce753aac83e4918c4eae24a199d81836c58362
RuyuYou <727809166@qq.com>
2017-05-27 07:21:36 +0800

    new

--- client/src/component/reports.js
+++ client/src/component/reports.js
@@ -1,33 +1,33 @@
 import {Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
+import '../../style/report.less';
 
-class TrainListHeader extends Component {
-
-  addTrain() {
-    const pathName = window.location.pathname;
-    this.props.router.push(pathName + '/new');
-  }
+class Reports extends Component {
 
   render() {
     const href = `/report/train`;
     return (
-      <div>
-        <div className='trainList-header'>
+      <div id="report">
+        <div className='report-header'>
           报表管理
         </div>
 
-        <div className='trainList-title'>
-          <button className='btn btn-default pull-right'>
-            <a href={href}><i className='fa fa-share'></i>
-              <span className='text'>车次报表 </span>
-            </a>
-          </button>
-          <button className='btn btn-default pull-right'>
-            <a href={href}><i className='fa fa-share'></i>
-              <span className='text'>车次报表 </span>
-            </a>
-          </button>
+        <div className='report-title row no-margin'>
+          <div className="col-sm-offset-2 col-sm-4">
+            <button className='btn btn-default'>
+              <a href={href}><i className='fa fa-share'></i>
+                <span className='text'>车次报表 </span>
+              </a>
+            </button>
+          </div>
+          <div className="col-sm-offset-2 col-sm-4">
+            <button className='btn btn-default'>
+              <a href={href}><i className='fa fa-share'></i>
+                <span className='text'>车次报表 </span>
+              </a>
+            </button>
+          </div>
         </div>
       </div>
     );
@@ -36,4 +36,4 @@ class TrainListHeader extends Component {
 
 const mapStateToProps = (state) => state;
 
-export default connect(mapStateToProps)(withRouter(TrainListHeader));
+export default connect(mapStateToProps)(withRouter(Reports));

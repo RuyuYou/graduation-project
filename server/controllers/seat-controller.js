61342f3de24d9fbf3580fed3158267df68a10e69
RuyuYou <727809166@qq.com>
2017-05-27 05:37:47 +0800

    new commit

--- server/controllers/seat-controller.js
+++ server/controllers/seat-controller.js
@@ -3,12 +3,57 @@ const Seat = require('../models/seat');
 const constant = require('../../config/constant');
 
 class SeatController {
+  getAll(req, res, next) {
+    Seat.find({}, (err, result)=> {
+      if (err) {
+        return next(err);
+      }
+      return res.status(constant.httpCode.OK).send(result);
+    });
+  }
+
+  getOne(req, res, next) {
+    Seat.findById(req.params.seatId, (err, result)=> {
+      if (err) {
+        return next(err);
+      }
+      if (!result) {
+        return res.sendStatus(constant.httpCode.NO_CONTENT);
+      }
+      return res.status(constant.httpCode.OK).send(result);
+    });
+  }
+
   create(req, res, next) {
     Seat.create(req.body, (err, result)=> {
       if (err) {
         return next(err);
       }
       return res.sendStatus(constant.httpCode.CREATED);
+    });
+  }
+
+  delete(req, res, next) {
+    Seat.findByIdAndRemove(req.params.seatId, (err, result)=> {
+      if (err) {
+        return next(err);
+      }
+      if (!result) {
+        return res.sendStatus(constant.httpCode.NO_CONTENT);
+      }
+      return res.sendStatus(constant.httpCode.OK);
+    });
+  }
+
+  update(req, res, next) {
+    Seat.findByIdAndUpdate(req.params.seatId, req.body, (err, result)=> {
+      if (err) {
+        return next(err);
+      }
+      if (!result) {
+        return res.sendStatus(constant.httpCode.NO_CONTENT);
+      }
+      return res.sendStatus(constant.httpCode.OK);
     });
   }
 }
--- server/routes/routers/seats.js
+++ server/routes/routers/seats.js
@@ -5,10 +5,10 @@ const SeatController = require('../../controllers/seat-controller');
 const router = Router();
 const seatCtrl = new SeatController();
 
-// router.get('/', seatCtrl.getAll);
-// router.get('/:seatId', seatCtrl.getOne);
+router.get('/', seatCtrl.getAll);
+router.get('/:seatId', seatCtrl.getOne);
 router.post('/', seatCtrl.create);
-// router.delete('/:seatId', seatCtrl.delete);
-// router.put('/:seatId', seatCtrl.update);
+router.delete('/:seatId', seatCtrl.delete);
+router.put('/:seatId', seatCtrl.update);
 
 module.exports = router;

b71c4bc8640acc35d474336c2fb5f61bae8255bb
RuyuYou <727809166@qq.com>
2017-05-28 15:32:42 +0800

    n

--- client/src/component/trainList/trainListHeader.js
+++ client/src/component/trainList/trainListHeader.js
@@ -21,12 +21,8 @@ class TrainListHeader extends Component {
           <button className='btn btn-default'
                   onClick={this.addTrain.bind(this)}>
             <i className='fa fa-plus blue'> </i>
-            <span className='text'>新增车次 </span>
+            <span className='text'>新增临时车次 </span>
           </button>
-          {/*<button className='btn btn-default' disabled="disabled">*/}
-          {/*<i className='fa fa-trash-o red'> </i>*/}
-          {/*<span className='text'>批量删除 </span>*/}
-          {/*</button>*/}
           <button className='btn btn-default pull-right'>
             <a href={href}><i className='fa fa-share'></i>
               <span className='text'>生成报表 </span>
--- client/src/component/trainNew/train-new-header.js
+++ client/src/component/trainNew/train-new-header.js
@@ -4,7 +4,7 @@ export default class TrainNewHeader extends Component {
 
   render() {
     return (<div className="train-editor-header">
-      {`${this.props.title}车次`}
+      {`${this.props.title}临时车次`}
     </div>);
   }
 }

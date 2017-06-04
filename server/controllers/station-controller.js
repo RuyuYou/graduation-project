831f93ba1c73151a94ba666c0543ac024eab7f0c
RuyuYou <727809166@qq.com>
2017-06-04 16:12:41 +0800

    new commit

--- server/controllers/station-controller.js
+++ server/controllers/station-controller.js
@@ -12,13 +12,29 @@ class StationController {
       if (!result) {
         return res.sendStatus(constant.httpCode.NO_CONTENT);
       }
+      res.clearCookie('trainId');
+      res.cookie('trainId', result.trainId);
       return res.status(constant.httpCode.OK).send(result.stations);
     });
   }
 
-  // getOneStation(req, res, next) {
-  //
-  // }
+  getOneStation(req, res, next) {
+    const trainId = req.cookies.trainId;
+    const number = req.params.number;
+    Station.findOne({trainId}, (err, result)=> {
+      if (err) {
+        return next(err);
+      }
+      if (!result) {
+        return res.sendStatus(constant.httpCode.NO_CONTENT);
+      }
+      result.stations.map((item, index)=> {
+        if (item.number == number) {
+          return res.status(constant.httpCode.OK).send(item);
+        }
+      });
+    });
+  }
 }
 
 module.exports = StationController;
--- server/routes/routers/stations.js
+++ server/routes/routers/stations.js
@@ -6,7 +6,7 @@ const router = Router();
 const stationCtrl = new StationController();
 
 router.get('/:trainId', stationCtrl.getTrainStation);
-// router.get('/:trainId/:number', stationCtrl.getOneStation);
+router.get('/:trainId/:number', stationCtrl.getOneStation);
 // router.post('/:trainId', stationCtrl.createStation);
 // router.delete('/:trainId/:number', stationCtrl.deleteOneStation);
 // router.put('/:trainId/:number', stationCtrl.updateStation);

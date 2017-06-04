a2ee309c2472e89d660634e4d147a3dacc6bcb6b
RuyuYou <727809166@qq.com>
2017-06-04 19:40:13 +0800

    new

--- server/controllers/ticker-controller.js
+++ server/controllers/ticker-controller.js
@@ -1,19 +1,9 @@
 const Tickers = require('../models/ticker');
-const Train = require('../models/train');
 
 const constant = require('../../config/constant');
 
 class TickerController {
-  getTickers(req, res, next) {
-    Tickers.find({}, (err, result) => {
-      if (err) {
-        return next(err);
-      }
-      return res.status(constant.httpCode.OK).send(result);
-    });
-  }
-
-  getOneTicker(req, res, next) {
+  getOneTrain(req, res, next) {
     const trainId = req.params.trainId;
     Tickers.findOne({trainId}, (err, result)=> {
       if (err) {
@@ -21,46 +11,6 @@ class TickerController {
       }
       if (!result) {
         return res.sendStatus(constant.httpCode.NO_CONTENT);
-      }
-      return res.status(constant.httpCode.OK).send(result);
-    });
-  }
-
-  createTickers(req, res, next) {
-    Tickers.findOne({trainId: req.body.trainId}, (err, result)=> {
-      if (result) {
-        return res.sendStatus(constant.httpCode.NO_CONTENT);
-      } else {
-        Tickers.create(req.body, (err, result) => {
-          if (err) {
-            return next(err);
-          }
-          return res.sendStatus(constant.httpCode.CREATED);
-        });
-      }
-    });
-  }
-
-  deleteTickers(req, res, next) {
-    const tickerId = req.params.tickerId;
-    Tickers.findByIdAndRemove(tickerId, (err, result)=> {
-      if (err) {
-        return next(err);
-      }
-      if (!result) {
-        return res.sendStatus(constant.httpCode.NOT_FOUND);
-      }
-      return res.sendStatus(constant.httpCode.NO_CONTENT);
-    });
-  }
-
-  updateTickers(req, res, next) {
-    Tickers.findOneAndUpdate(req.params.tickerId, req.body, (err, result)=> {
-      if (err) {
-        return next(err);
-      }
-      if (!result) {
-        return res.sendStatus(constant.httpCode.NOT_FOUND);
       }
       return res.status(constant.httpCode.OK).send(result);
     });
--- server/routes/routers/tickers.js
+++ server/routes/routers/tickers.js
@@ -4,10 +4,9 @@ const TickerController = require('../../controllers/ticker-controller');
 const router = Router();
 const tickerCtrl = new TickerController();
 
-router.get('/', tickerCtrl.getTickers);
-router.get('/:trainId', tickerCtrl.getOneTicker);
-router.put('/:tickerId', tickerCtrl.updateTickers);
-router.post('/', tickerCtrl.createTickers);
-router.delete('/:tickerId', tickerCtrl.deleteTickers);
+router.get('/:trainId', tickerCtrl.getOneTrain);
+// router.put('/:tickerId', tickerCtrl.updateTickers);
+// router.post('/', tickerCtrl.createTickers);
+// router.delete('/:tickerId', tickerCtrl.deleteTickers);
 
 module.exports = router;
